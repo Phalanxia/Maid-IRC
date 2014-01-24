068cc0ccafab1f9fec69abf05bb9f0b17391e470
Madison Tries <Phalanxia@users.noreply.github.com>
2014-01-23 23:18:27 -0600

    Continued work on the client

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log("Starting");
+console.log("Starting MaidIRC");
 
 var ircLib = require('irc'),
 	express = require('express'),
@@ -26,7 +26,11 @@ var server = http.createServer(app)
 	.listen(config.http_port, config.http_host);
 
 app.get('/', function (req, res) {
-	res.render("index", {}); // Change "index" to "client" to test the client without having to connect to IRC.
+	res.render("index", {});
+});
+
+app.get('/preview', function (req, res) {
+	res.render("client", {}); // Go to /preview to preview the client without connecting to IRC.
 });
 
 // Set up socket.io
@@ -46,69 +50,83 @@ app.post('/client', function (req, res) {
 
 	res.render("client", {server: req.body.server, name: req.body.name, channel: req.body.channel});
 
-	var theChannel = [req.body.channel]
-	console.log(theChannel);
-
-	var client = new ircLib.Client(req.body.server, req.body.name, {
-		channels: theChannel,
-		userName: req.body.name,
-		password: null,
-		realName: "MadiIRC",
-		floodProtection: true,
-		floodProtectionDelay: 1000,
-		autoRejoin: true,
-		autoConnect: true,
-		stripColors: true
-	});
-
-	// IRC Listeners
-	client.addListener('registered', function (message) {
-		console.log('Server: ' + message.server);
-		console.log(message.args[1]);
-	});
-
-	client.addListener('message', function (from, to, message) {
-		console.log(to + ' => ' + from + ': ' + message);
-		if (message == 'say meow') {
-			client.say('##phalanxia', 'nyan~');
-		}
-	});
-
-	client.addListener('join', function (from, to, message) {
-		data = ['join', from, to, message];
-		io.sockets.emit('join/part', data);
-	});
-
-	client.addListener('part', function (from, to, message) {
-		data = ['part', from, to, message];
-		io.sockets.emit('join/part', data);
-	});
-
-	client.addListener('error', function (message) {
-		console.log('error: ', message);
-	});
-
-	// And now the rest of the Socket.io code.
 	io.sockets.on('connection', function (socket) {
-		// Emit
+		var theChannel = [req.body.channel];
+			console.log(theChannel);
 
+		var client = new ircLib.Client(req.body.server, req.body.name, {
+			channels: theChannel,
+			userName: req.body.name,
+			password: null,
+			realName: "MaidIRC",
+			floodProtection: true,
+			floodProtectionDelay: 1000,
+			autoRejoin: true,
+			autoConnect: true,
+			stripColors: true
+		});
+
+		var settings = {
+			nickname: req.body.name,
+			channels: []
+		}
+
+		// IRC Listeners
+		client.addListener('registered', function (message) {
+			console.log('Server: ' + message.server);
+			console.log(message.args[1]);
+		});
+
+		client.addListener('names', function (data) {
+			console.log(data);
+		});
+
+		client.addListener('message', function (from, to, message) {
+			console.log(to + ' => ' + from + ': ' + message);
+			socket.emit('recieveMessage', [to, from, message]);
+		});
+
+		client.addListener('join', function (from, to, message) {
+			data = ['join', from, to, message];
+			io.sockets.emit('join/part', data);
+		});
+
+		client.addListener('part', function (from, to, message) {
+			data = ['part', from, to, message];
+			io.sockets.emit('join/part', data);
+		});
+
+		client.addListener('error', function (message) {
+			console.log('error: ', message);
+		});
+
+		// And now the rest of the Socket.io code.
+		
 		// Recieved
 		socket.on('shutdown', function (data) {
+			console.log("working 1");
+			//client.disconnect("Quit");
 			setTimeout(function () {
 				console.log('Exiting.');
 				process.exit(0);
 			}, 100);
 		});
 
-		socket.on('server', function (data) {
+		socket.on('disconnect', function () {
+			console.log("Client disconnected");
+			client.disconnect("Quit");
+		});
+
+		// IRC Emit
+		socket.emit('ircInfo', settings);
+
+		// IRC Recieve
+		socket.on('sendServer', function (data) {
 			console.log(data);
 		});
 
-		// IRC
-		socket.on('message' , function (data) {
+		socket.on('sendMessage' , function (data) {
 			client.say(data[0], data[1]);
 		});
 	});
-
-	return client;
 });
