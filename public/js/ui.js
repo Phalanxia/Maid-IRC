d41efe9c2c812de3c18bd54d4ec4ae6f363b70df
Madison Tries <xphalanxia@gmail.com>
2014-01-29 13:50:37 -0600

    New client and a lot of other stuff I don't even remember

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,49 +1,5 @@
-function resizeUI() {
-	h = $('body').height();
-	w = $('body').width();
-
-	$('#sidebar').css({
-		"height": h
-	});
-
-	$('#rightSide').css({
-		"height": h,
-		"width": w - 201
-	});
-
-	// Backups to make sure the layouts don't break >:
-	$('#sidebar nav, #sidebar nav ul').css({
-		"height": h - 130
-	});
-
-	$('#consoleOutput').css({
-		"height": h - 144
-	});
-
-	$('#consoleInput, #consoleLeft, #consoleOutput').css({
-		"width": w - (202 + $('#sidebar').width())
-	});
-
-	$('#users ul').css({
-		"height": h - 164
-	});
-
-	$('#webConsole output').css({
-		"height": $('#webConsole').height() - 40
-	});
-}
-
-$(window).resize(function(){
-	resizeUI();
-});
-
-// On loaded
-$(function () {
-	resizeUI();
-});
-
 $(window).bind('beforeunload', function () {
-	return "You have attempted to leave this page. Doing so will disconnect you from IRC. \n Are you sure you want to exit this page?";
+	return "You have attempted to leave this page. Doing so will disconnect you from IRC.";
 });
 
 $('button#shutdown').click(function () {
