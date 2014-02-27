947c77e6a5197293ac0e705800d960887eacaec3
Madison Tries <Phalanxia@users.noreply.github.com>
2014-02-26 22:39:01 -0600

    Removed jquery.
    Also fixed a misspelling in client.less... Thank you eli. Etc.

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,17 @@
-$('input[type=text]').focus(function () {
-	$(this).parent().addClass('outline');
-}).blur(function (){
-	$(this).parent().removeClass('outline');
-});
-
-$('input[type=password]').focus(function () {
-	$(this).parent().addClass('outline');
-}).blur(function (){
-	$(this).parent().removeClass('outline');
-});
+var select = function (selectors) {
+	if (selectors.indexOf(',') != -1) {
+		return document.querySelectorAll(selectors);
+	} else {
+		return document.querySelector(selectors);
+	}
+};
 
 var advanced = false;
 
-$('a#advanced').click(function () {
+select('a#advanced').onclick = function () {
 	if (!advanced) {
 		advanced = true;
-		$('section:nth-of-type(2)').after('<section class="checkbox"><input id="ssl" type="checkbox" name="sslToggle"><span class="checkboxLabel">SSL</span></section>');
-		$('section:nth-of-type(1)').after('<section class="bubble"><div class="nipple"><span></span></div><input id="nicknamePassword" type="password" placeholder="Password" name="nicknamePassword"></section><section class="bubble"><input id="realName" type="text" placeholder="Real Name" name="realName"></section>');
+		select('section:nth-of-type(2)').insertAdjacentHTML('afterend', '<section class="checkbox"><input id="ssl" type="checkbox" name="sslToggle"><span class="checkboxLabel">SSL</span></section>');
+		select('section:nth-of-type(1)').insertAdjacentHTML('afterend', '<section class="bubble"><div class="nipple"><span></span></div><input id="nicknamePassword" type="password" placeholder="Password" name="nicknamePassword"></section><section class="bubble"><input id="realName" type="text" placeholder="Real Name" name="realName"></section>');
 	}
-});
+};
