28a55530538b345e1109bbe2b5c8da1f63d40235
Madison Tries <Phalanxia@users.noreply.github.com>
2014-03-20 15:24:04 -0500

    New login page styles

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,6 @@ select('a#advanced').onclick = function () {
 	if (!advanced) {
 		advanced = true;
 		select('section:nth-of-type(2)').insertAdjacentHTML('afterend', '<section class="checkbox"><input id="ssl" type="checkbox" name="sslToggle"><span class="checkboxLabel">SSL</span></section>');
-		select('section:nth-of-type(1)').insertAdjacentHTML('afterend', '<section class="bubble"><div class="nipple"><span></span></div><input id="nicknamePassword" type="password" placeholder="Password" name="nicknamePassword"></section><section class="bubble"><input id="realName" type="text" placeholder="Real Name" name="realName"></section>');
+		select('section:nth-of-type(1)').insertAdjacentHTML('afterend', '<section><input id="nicknamePassword" type="password" placeholder="Password" name="nicknamePassword"></section><section><input id="realName" type="text" placeholder="Real Name" name="realName"></section><hr />');
 	}
 };
