6d8b0bf6313967bf4589b49394cf07f4a4cec315
Madison Tries <Phalanxia@users.noreply.github.com>
2014-12-13 07:26:39 -0600

    Commit

--- config.js
+++ config.js
@@ -3,7 +3,10 @@ config = {
 	logging_directory: '',
 	// Host/Port which you want the webserver to serve on.
 	http_host: process.env.HOST || '0.0.0.0', // Change to '0.0.0.0' to make this public.
-	http_port: process.env.PORT || 80
+	http_port: process.env.PORT || 80,
+	// Https settings
+	enable_https: 0, // 0 = false, 1 = true, 2 = force
+	https_port: 443
 };
 
 module.exports = config;
