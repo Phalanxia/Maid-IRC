d341b9d7873d9dab507f4077df215f54e72d4e47
Madison Tries <Phalanxia@users.noreply.github.com>
2015-10-10 06:55:26 -0500

    Fixed sending messages, updated message source icons and indentation, long names get clipped off, style fixes, 412 443

--- public/js/templates.js
+++ public/js/templates.js
@@ -1,6 +1,6 @@
 var Templates = {
 	messageSource: {
-		source: '<ul class="message-source-list"><li class="server" data-connection-id="{{connectionId}}" data-value="SERVER"><h2>{{serverName}}</h2></li>{{#with sources}}<li class="channel" data-connection-id="{{data.connectionId}}" data-value="{{key}}" data-alert=""><i class="fa fa-comments-o"></i><span>{{key}}</span></li>{{/with}}</ul>'
+		source: '<ul class="message-source-list"><li class="server" data-connection-id="{{connectionId}}" data-value="SERVER"><i class="fa fa-server"></i><span>{{serverName}}</span></li>{{#with sources}}<li class="channel" data-connection-id="{{data.connectionId}}" data-value="{{key}}" data-alert=""><i class="fa fa-comments-o"></i><span>{{key}}</span></li>{{/with}}</ul>'
 	},
 	userList: {
 		source: '<li><p data-rank="{{rank}}" data-rank-icon="{{{icon}}}">{{nick}}</p></li>'
